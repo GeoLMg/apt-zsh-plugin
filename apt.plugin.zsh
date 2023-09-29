a273ba8f07247ecf017445c8f45672aa4d3eacb2
GeoLMg <7022785g@gmail.com>
2023-09-30 01:08:10 +0300

    Plugin is ready for usage, fixed next line bug

--- apt.plugin.zsh
+++ apt.plugin.zsh
@@ -3,10 +3,11 @@ function command_not_found_handler() {
   RED='\033[0;31m'
   Green='\033[0;32m' 
   NC='\033[0m' # No Color
-  echo -e "${RED}Command \"$0\" not found${NC}.\nDo you want to install it? [${Green}y$NC|${RED}N$NC]"
+  echo -en "${RED}Command \"$0\" not found${NC}.\nDo you want to install it? [${Green}y$NC|${RED}N$NC]: "
   read -k yn
+  echo ""
   case $yn in
-    [yY]*)
+    [дД]|[yY]|[eE]|[sS]|[yY]*|[jJ]*)
              sudo apt install $0 -y
             ;;
         *)
